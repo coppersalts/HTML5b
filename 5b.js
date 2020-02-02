d3d70119873e0a3d6eec5ea704e739a32239c6d0
SceinceSean <seansscience@gmail.com>
2020-02-02 16:50:09 -0600

    Added menu image and flash effect.

--- 5b.js
+++ 5b.js
@@ -8,11 +8,15 @@ var pmouseIsPressed = false;
 
 var menu = 0;
 
-var buttonWatch5a = new button(670, 308, 264, 27.5, 5, '#fff', '#d4d4d4', '#b8b8b8', 'WATCH BFDIA 5a');
-var buttonNewGame = new button(670, 353, 264, 27.5, 5, '#fff', '#d4d4d4', '#b8b8b8', 'NEW GAME');
-var buttonContinueGame = new button(670, 398, 264, 27.5, 5, '#fff', '#d4d4d4', '#b8b8b8', 'CONTINUE GAME');
-var buttonLevelCreator = new button(670, 443, 264, 27.5, 5, '#fff', '#d4d4d4', '#b8b8b8', 'LEVEL CREATOR');
-var buttonExplore = new button(670, 488, 264, 27.5, 5, '#fff', '#d4d4d4', '#b8b8b8', 'EXPLORE');
+var menu0img = new Image();
+
+var flashEffect = 1;
+
+var buttonWatch5a = new button(670, 308, 264, 27.5, 5, '#fff', '#d4d4d4', '#b8b8b8', 'WATCH BFDIA 5a', 30);
+var buttonNewGame = new button(670, 353, 264, 27.5, 5, '#fff', '#d4d4d4', '#b8b8b8', 'NEW GAME', 30);
+var buttonContinueGame = new button(670, 398, 264, 27.5, 5, '#fff', '#d4d4d4', '#b8b8b8', 'CONTINUE GAME', 30);
+var buttonLevelCreator = new button(670, 443, 264, 27.5, 5, '#fff', '#d4d4d4', '#b8b8b8', 'LEVEL CREATOR', 30);
+var buttonExplore = new button(670, 488, 264, 27.5, 5, '#939393', '#868686', '#7e7e7e', 'EXPLORE', 30);
 
 function setup() {
 	createCanvas(width, height);
@@ -20,6 +24,9 @@ function setup() {
 	document.getElementById('center').insertBefore(canvas,document.getElementsByTagName('p')[0]);
 	ctx = canvas.getContext('2d');
 	pixelDensity(pixelRatio);
+
+	menu0img.onload = function() {ctx.drawImage(menu0img, 0, 0);}
+	menu0img.src = 'imgs/menu 0.png';
 }
 
 function draw() {
@@ -28,7 +35,7 @@ function draw() {
 	ctx.fillStyle = '#666';
 	ctx.fillRect(0, 0, width, height);
 
-	if (menu == 0){
+	if (menu == 0) {
 		buttonWatch5a.show();
 		buttonWatch5a.update();
 		buttonNewGame.show();
@@ -40,15 +47,27 @@ function draw() {
 		buttonExplore.show();
 		buttonExplore.update();
 
-		if (buttonContinueGame.relesed()){
+		if (buttonContinueGame.relesed()) {
 			menu = 1;
 		}
+		if (buttonWatch5a.relesed()) {
+			window.open('https://www.youtube.com/watch?v=4q77g4xo9ic');
+		}
+		if (buttonLevelCreator.relesed()) {
+			window.open('https://zolo101.github.io/BFDIA5bLevelEditor/');
+		}
+
+		ctx.drawImage(menu0img, 0, 0, width, height);
 	}
 
+	ctx.fillStyle = 'rgba(255,255,255,'+flashEffect+')';
+	ctx.fillRect(0, 0, width, height);
+
+	if (flashEffect>0) flashEffect-=0.001;
 	pmouseIsPressed = mouseIsPressed
 }
 
-function button(x, y, w, h, cr, fillNormal, fillHover, fillPress, text) {
+function button(x, y, w, h, cr, fillNormal, fillHover, fillPress, text, fontSize) {
 	this.fillNormal = fillNormal;
 	this.fillHover = fillHover;
 	this.fillPress = fillPress;
@@ -73,52 +92,50 @@ function button(x, y, w, h, cr, fillNormal, fillHover, fillPress, text) {
 		ctx.lineTo(this.x - this.cr, this.y);
 		ctx.fill();
 
-		ctx.font = 'bold 30px Arial';
+		ctx.font = 'bold '+fontSize+'px Helvetica';
 		ctx.fillStyle = '#666';
 		ctx.textAlign = 'center';
 		ctx.fillText(this.text, this.x + (this.w/2), this.y + (this.h*0.9));
 	}
 
-	this.update = function(){
+	this.update = function() {
 		this.hover();
-		if(this.pressed()){
+		if (this.pressed()) {
 			this.held = true;
-		} else if(!mouseIsPressed){
+		} else if (!mouseIsPressed) {
 			this.held = false;
 		}
 	}
 
 	this.hover = function() {
-		if (this.hovering(mouseX, mouseY)){
+		if (this.hovering(mouseX, mouseY)) {
 			this.fill = this.fillHover;
-			if(mouseIsPressed){
+			if (mouseIsPressed) {
 				this.fill = this.fillPress;
 			}
-		} else if(this.held && !this.hovering(mouseX, mouseY)){
+		} else if (this.held && !this.hovering(mouseX, mouseY)) {
 			this.fill = this.fillHover;
 		} else {
 			this.fill = this.fillNormal;
 		}
 	}
 
-	this.hovering = function(x, y){
+	this.hovering = function(x, y) {
 		return x>this.x && x<this.x+this.w && y>this.y && y<this.y+this.h;
 	}
 
-	this.pressed = function(){
+	this.pressed = function() {
 		return this.hovering(mouseX, mouseY) && mouseIsPressed;
 	}
 
-	this.ppressed = function(){
+	this.ppressed = function() {
 		return this.hovering(pmouseX, pmouseY) && pmouseIsPressed;
 	}
 
-	this.relesed = function(){
+	this.relesed = function() {
 		return this.ppressed() && !this.pressed() && !this.held;
 	}
 }
-
-// https://www.youtube.com/watch?v=4q77g4xo9ic
 
 setup();
 draw();
