009cfde97d34caf6b57d32299160d361578bcefe
ScienceSean <seansscience@gmail.com>
2022-02-22 18:41:11 -0600

    win tokens stopped rendering so I changed a false to a true to make it work again

--- 5b.js
+++ 5b.js
@@ -249,7 +249,7 @@ var blockProperties = [
 	// tile1
 	[true,true,true,true,false,false,false,false,false,false,false,0,0,true,false,true,1,false],
 	[false,false,false,false,false,false,false,false,false,false,true,0,0,false,false,true,1,false],
-	[false,false,false,false,false,false,false,false,false,true,false,0,0,false,false,true,0,false],
+	[false,false,false,false,false,false,false,false,true,true,false,0,0,false,false,true,0,false],
 	[true,true,true,true,false,false,false,false,true,false,false,0,0,false,false,true,14,false,[0,1,2,3,4,5,6,7,8,9,10,11,12,13]],
 	[true,true,true,true,false,false,false,false,true,false,false,0,6,false,false,true,12,true,[0,1,2,3,4,5,6,7,8,9,10,11]],
 	[false,false,false,false,false,false,false,false,true,false,true,0,0,false,false,true,41,true,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]],
