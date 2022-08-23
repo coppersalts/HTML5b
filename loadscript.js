667d34c6d519ecc00d9ddaea3a925c7e4c0cd567
Ima Person <me@imaperson.dev>
2022-08-22 22:24:32 -0400

    Update 5b.min.js

--- loadscript.js
+++ loadscript.js
@@ -1,5 +1,5 @@
 const script = document.getElementById('5bscript');
-script.src = localStorage.getItem('script') || '5b.js';
+script.src = localStorage.getItem('script') || '5b.min.js';
 
 document.onreadystatechange = () => {
     if (localStorage.getItem('script') && localStorage.getItem('script') != '5b.js') {
