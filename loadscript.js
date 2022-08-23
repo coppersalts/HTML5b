aaa6d585e294c7dda9d6bcbadbf589b4b8637d52
Ima Person <me@imaperson.dev>
2022-08-22 22:43:12 -0400

    Bugfix
    mb lol

--- loadscript.js
+++ loadscript.js
@@ -2,7 +2,7 @@ const script = document.getElementById('5bscript');
 script.src = localStorage.getItem('script') || '5b.min.js';
 
 document.onreadystatechange = () => {
-    if (localStorage.getItem('script') && localStorage.getItem('script') != '5b.js') {
+    if (localStorage.getItem('script') && localStorage.getItem('script') != '5b.min.js') {
         document.getElementById('resetmod').style.visibility = 'visible';
     }
 }
