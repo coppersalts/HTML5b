0269aac9aa051b7b38d1829291aab40063ecd994
ScienceSean <seansscience@gmail.com>
2022-10-06 23:05:18 -0500

    added easter egg you don't need to worry about

--- 5b.js
+++ 5b.js
@@ -7697,6 +7697,14 @@ function draw() {
 				}
 			}
 
+			// This is an easter egg I added for the wiki camp 2. You can ignore it.
+			if (onRect(_xmouse, _ymouse, 674, 142, 30, 30)) {
+				onButton = true;
+				if (!mouseIsDown && pmouseIsDown) {
+					window.open('https://camp2.rectangle.zone/index.php?title=5b_Challenge_Crystal');
+				}
+			}
+
 			qTimer--;
 			x = -cameraX;
 			y = -cameraY;
